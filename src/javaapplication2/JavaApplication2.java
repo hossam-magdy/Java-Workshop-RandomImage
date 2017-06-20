38758eb43cc2a9c4c89393adbe9a7c7d423b00d5
Hossam Magdy <hossam.magdy@3ddx.com>
2017-06-20 13:27:05 +0200

    Stuck at sibling object method calling

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -11,6 +11,7 @@ package javaapplication2;
  */
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import javax.swing.*;
 import java.util.Random;
 import javax.swing.border.Border;
@@ -18,7 +19,8 @@ import javax.swing.border.Border;
 public class JavaApplication2 {
 
     private JFrame myFrame;
-    private JLabel myLabel;
+    private pixelsPanel pixelsPanel;
+    private statusBarPanel statusBarPanel;
     public static int WIDTH = 800;
     public static int HEIGHT = 600;
     public static int HEIGHT_bar = 100;
@@ -38,13 +40,15 @@ public class JavaApplication2 {
 
         myFrame.setLayout(new BorderLayout());
 
-        pixelsPanel pixelsPanel = new pixelsPanel();
+        pixelsPanel = new pixelsPanel();
         pixelsPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT - HEIGHT_bar));
         myFrame.add(pixelsPanel);
+        pixelsPanel.pixelsPanel();
 
-        statusBarPanel statusBarPanel = new statusBarPanel();
+        statusBarPanel = new statusBarPanel();
         myFrame.add(statusBarPanel, BorderLayout.PAGE_END);
         statusBarPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT_bar));
+        statusBarPanel.renderComponents();
 
         myFrame.setVisible(true);
 
@@ -59,15 +63,55 @@ public class JavaApplication2 {
 
 }
 
-class pixelsPanel extends JPanel {
+class pixelsPanel extends JPanel implements MouseListener {
+
+    public JLabel statusLabel;
+
+    public void pixelsPanel() {
+        this.addMouseListener(this);
+        statusLabel = new JLabel("TEST", JLabel.CENTER);
+        //statusLabel.setBackground(Color.red);
+        add(statusLabel);
+    }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.YELLOW);
-        g2.fillRect(0, 0, JavaApplication2.WIDTH, JavaApplication2.HEIGHT - JavaApplication2.HEIGHT_bar);
         this.setBackground(Color.red);
+        BufferedImage image = new BufferedImage(JavaApplication2.WIDTH, JavaApplication2.HEIGHT, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < JavaApplication2.WIDTH; x++) {
+            for (int y = 0; y < JavaApplication2.HEIGHT; y++) {
+                image.setRGB(x, y, JavaApplication2.randomColor().getRGB());
+            }
+        }
+        g.drawImage(image, 0, 0, JavaApplication2.WIDTH, JavaApplication2.HEIGHT - JavaApplication2.HEIGHT_bar, null);
+        //pixelsPanel.add(image);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        statusLabel.setText(Integer.toString(e.getX()));
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
 
@@ -75,7 +119,7 @@ class statusBarPanel extends JPanel {
 
     private JLabel myLabel;
 
-    public void statusBar() {
+    public void renderComponents() {
         myLabel = new JLabel("TEST", JLabel.CENTER);
 
         FlowLayout myLayout = new FlowLayout();
@@ -85,6 +129,22 @@ class statusBarPanel extends JPanel {
         this.setSize(JavaApplication2.WIDTH, JavaApplication2.HEIGHT_bar);
     }
 
+    /*
+    public void highlight(Point cursor) {
+        for (int i = 0; i < buttons.length; i++) {
+            JButton button = buttons[i];
+            Point buttonLocation = button.getLocationOnScreen();
+            double west = buttonLocation.getX();
+            double east = buttonLocation.getX() + button.getWidth();
+            double north = buttonLocation.getY();
+            double south = buttonLocation.getY() + button.getHeight();
+            boolean inRow = cursor.getX() > west && cursor.getX() < east;
+            boolean inCol = cursor.getY() > north && cursor.getY() < south;
+            boolean inBounds = inRow || inCol;
+            button.setBackground(inBounds ? new Color(0xFFFF00) : null);
+        }
+    }
+     */
     public void setText(String text) {
         myLabel.setText(text);
     }
